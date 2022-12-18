872d9fbf032da4d448652e0b552df09c5f379b93
赤羽美輝 <118857747+akabane-meifai@users.noreply.github.com>
2022-12-18 13:36:01 +0900

    メソッドを変更

--- Mixin.js
+++ Mixin.js
@@ -1,14 +1,35 @@
 class Mixin{
 	constructor(...args){
 		this.si = [];
+		this.funcs = {};
 		for(let sc of this.constructor.sc){
+			let c = sc;
+			let i;
 			if(Array.isArray(sc)){
+				c = sc[0];
 				let a = sc.slice(1).map(i => args[i]);
-				this.si.push(new sc[0](...a));
+				this.si.push(i = new c(...a));
 			}else{
-				this.si.push(new sc());
+				this.si.push(i = new sc());
+			}
+			const props = Reflect.ownKeys(c.prototype).filter(p => (p != "constructor") && (typeof c.prototype[p] === "function"));
+			for(let prop of props){
+				if(!(prop in this.funcs)){
+					this.funcs[prop] = new Proxy(function(){}, {
+						apply: function(target, thisArg, argumentsList){
+							let res;
+							for(let item of target.chain){
+								res = item.func.apply((target.receiver == thisArg) ? item.instance : thisArg, argumentsList);
+							}
+							return res;
+						}
+					});
+					this.funcs[prop].chain = [];
+				}
+				this.funcs[prop].chain.push({func: c.prototype[prop], instance: i});
 			}
 		}
+		console.log(this.funcs);
 	}
 	static proxy = Symbol("proxy");
 	static attach(...classes){
@@ -24,10 +45,11 @@ class Mixin{
 		const mixin = new this(...args);
 		const proxy = new Proxy(mixin, {
 			get(target, prop, receiver){
+				if(prop in target.funcs){
+					return Object.assign(target.funcs[prop], {receiver: receiver});
+				}
 				for(let si of target.si){
-					if(prop in si){
-						return (typeof si[prop] === "function") ? si[prop].bind(si) : si[prop];
-					}
+					return si[prop];
 				}
 			},
 			has(target, key){
